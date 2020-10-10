6d7b8673edcd494f927c43ff892dc4dcf12be20f
hideonbush4 <1917314396@qq.com>
2020-10-10 17:24:52 +0800

    init repo

---
+++ src/main/java/com/stt/community/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.stt.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @create 2020-10-10 14:40
+ */
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name="name")String name, Model model){
+        model.addAttribute("name",name);
+        return "hello";
+    }
+
+
+}
